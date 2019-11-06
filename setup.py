ac5e25ba4bf0867ea9a98c4e0c69cc1ea916ff97
bdvllrs <bdvllrs@gmail.com>
2019-11-06 14:47:56 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='pin',
-      version='0.0.1',
+      version='0.0.3',
       py_modules=['pin'],
       install_requires=['sacred', 'Click'],
       packages=find_packages(),
