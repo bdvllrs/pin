a30d619a425ff0b5aa00c1d1fda57a0f756f65c1
bdvllrs <bdvllrs@gmail.com>
2019-11-06 14:37:07 +0100

    Add prefix when logging to writer in Metrics.

--- pin/summary_writer.py
+++ pin/summary_writer.py
@@ -108,5 +108,5 @@ class Metrics:
         if self.writer is not None:
             for key in self.metrics.keys():
                 if len(self.metrics[key]):
-                    self.writer.add(key, self.metrics[key][-1])
+                    self.writer.add(self.prefix + key, self.metrics[key][-1])
 
