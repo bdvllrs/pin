749c5672d1fe4cfc13ad56da9e3fdc0108d1bf71
bdvllrs <bdvllrs@gmail.com>
2019-11-13 15:33:06 +0100

    Add explicit package data back.

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import find_packages, setup
 
 setup(name='pin',
-      version='0.0.4',
+      version='0.0.3',
       py_modules=['pin'],
       install_requires=['sacred', 'Click'],
       packages=find_packages(),
+      package_data={'pin': ['templates/config/default/*',
+                            'templates/*',
+                            'templates/scripts/*']},
       include_package_data=True,
       entry_points="""
       [console_scripts]
