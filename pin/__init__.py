cf736ebd13516e3bfc9aecdbba7fb8bcbc06f180
Benjamin <benjamin.devillers@supelec.fr>
2020-02-05 14:28:57 +0100

    Remove experiments in __init__.py.

--- pin/__init__.py
+++ pin/__init__.py
@@ -1,5 +1,4 @@
 from .constants import DEBUG
-from .experiments import control_randomness, set_cuda_device
 from .summary_writer import Metrics, SummaryWriter
 from .artifact import TorchModelArtifact
 from .config import load_config
